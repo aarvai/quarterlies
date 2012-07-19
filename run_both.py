5addecc324466ce0bbd7523f76158554b10c4597
aarvai <aarvai@cfa.harvard.edu>
2012-07-19 08:00:50 -0400

    Added saveas to LTTplot and Reordered
    Now have option to save fig as special name

--- run_both.py
+++ run_both.py
@@ -13,32 +13,6 @@ start = '2012:032:00:00:00'
 stop = '2012:150:00:00:00'
 
 ##-------------------------------------------------------------
-# Run Quarterlies
-
-pp.close('all')
-
-t = Time.DateTime()
-new_dir = t.date[:4] + '_' + t.date[5:8] + '_RUN'
-pcad_dir = 'pcad_' + t.date[:4] + '_' + t.date[5:8]
-prop_dir = 'prop_' + t.date[:4] + '_' + t.date[5:8]
-
-if ~path.exists(new_dir):
-    mkdir(new_dir)
-
-chdir(new_dir)
-if ~path.exists(pcad_dir):
-    mkdir(pcad_dir)
-chdir(pcad_dir)
-execfile('pcad_quarterly.py')
-chdir('..')
-
-if ~path.exists(prop_dir):
-    mkdir(prop_dir)
-    chdir(prop_dir)
-execfile('prop_quarterly.py')
-chdir('..')
-
-##-------------------------------------------------------------
 # LTTplot function
 
 def LTTplot(var, **kwargs): 
@@ -48,7 +22,8 @@ def LTTplot(var, **kwargs):
     :var:  MSID or derived parameter name (string)
     
     Optional inputs:
-    :savefig:      Save to a .png with MSID's name (default is True)
+    :savefig:      Save and close figure (default is True)
+    :saveas:       Name to save figure as (default is <MSID>.png)
     :start:        Start time (default is 2000:001)
     :stop:         Stop time (default is None)
     :stat:         Statistic type ('5min' or 'daily', default is 'daily')
@@ -65,7 +40,7 @@ def LTTplot(var, **kwargs):
     :cust_unit:    Custom unit, typically for use with custom_mult (default 
                    is none)
     :custom_mult:  Custom multiplier, typically for use with custom unit 
-                   (default is none)
+                   (default is none)                 
     e.g.
     LTTplot('Dist_SatEarth')
     LTTplot('pline05t', ylim=[30,170], savefig=False) 
@@ -74,8 +49,7 @@ def LTTplot(var, **kwargs):
     LTTplot('plaed3gt', filter=['2011:299:00:00:00 2011:300:00:00:00'])
     LTTplot('pcm01t', subplot=[2,1,1], savefig=False) 
     """
-    import filter_times as bad_times
-    
+       
     var = var.lower()
     start = kwargs.pop('start', '2000:001')
     stop = kwargs.pop('stop', None)
@@ -84,10 +58,10 @@ def LTTplot(var, **kwargs):
     GRAY = '#999999'
     if kwargs.has_key('filter'):
         data.filter_bad_times(table=kwargs.pop('filter'))
-    elif hasattr(bad_times, var):
-        data.filter_bad_times(table=getattr(bad_times, 'bad_' + var))
+    elif hasattr(bad, 'bad_' + var):
+        data.filter_bad_times(table=getattr(bad, 'bad_' + var))
     if kwargs.has_key('subplot'):
-        sp = kwargs.pop('subplot')
+        sp = kwargs.get('subplot')
         pp.subplot(sp[0],sp[1],sp[2])
     else:  
         pp.figure()
@@ -101,17 +75,18 @@ def LTTplot(var, **kwargs):
     if hasattr(data, 'tdb'):
         pp.title(data.msid.upper() + ':  ' + data.tdb.technical_name)
         if lim == True:
-            pp.plot(pp.xlim(), np.array([data.tdb.Tlmt[4], 
-                                         data.tdb.Tlmt[4]]), 'r')
-            pp.plot(pp.xlim(), np.array([data.tdb.Tlmt[2], 
-                                         data.tdb.Tlmt[2]]), 'y')
-            pp.plot(pp.xlim(), np.array([data.tdb.Tlmt[3], 
-                                         data.tdb.Tlmt[3]]), 'y')
-            pp.plot(pp.xlim(), np.array([data.tdb.Tlmt[5], 
-                                         data.tdb.Tlmt[5]]), 'r')  
-            if ~kwargs.has_key('ylim'):
-                pp.ylim(np.array([data.tdb.Tlmt[4]-10.0, 
-                                  data.tdb.Tlmt[5]+10.0]))
+            if size(data.tdb.Tlmt) == 1 and data.tdb.Tlmt is not None:
+                pp.plot(pp.xlim(), np.array([data.tdb.Tlmt[4], 
+                                             data.tdb.Tlmt[4]]), 'r')
+                pp.plot(pp.xlim(), np.array([data.tdb.Tlmt[2], 
+                                             data.tdb.Tlmt[2]]), 'y')
+                pp.plot(pp.xlim(), np.array([data.tdb.Tlmt[3], 
+                                             data.tdb.Tlmt[3]]), 'y')
+                pp.plot(pp.xlim(), np.array([data.tdb.Tlmt[5], 
+                                             data.tdb.Tlmt[5]]), 'r')  
+                if ~kwargs.has_key('ylim'):
+                    pp.ylim(np.array([data.tdb.Tlmt[4]-10.0, 
+                                      data.tdb.Tlmt[5]+10.0]))
     else: pp.title(data.msid.upper())
     if kwargs.has_key('cust_unit'):
         pp.ylabel(kwargs.pop('cust_unit'))
@@ -130,6 +105,33 @@ def LTTplot(var, **kwargs):
             pp.plot(pp.xlim(), np.array([r[i], r[i]]), 'r')        
     s = kwargs.pop('savefig', True)
     if s == True:
-        pp.savefig(data.msid.lower() + '.png')
+        figname = kwargs.pop('saveas', data.msid.lower() + '.png')
+        pp.savefig(figname)
         pp.close()
 
+##-------------------------------------------------------------
+# Run Quarterlies
+
+pp.close('all')
+
+t = Time.DateTime()
+new_dir = t.date[:4] + '_' + t.date[5:8] + '_RUN'
+pcad_dir = 'pcad_' + t.date[:4] + '_' + t.date[5:8]
+prop_dir = 'prop_' + t.date[:4] + '_' + t.date[5:8]
+
+if not path.exists(new_dir):
+    mkdir(new_dir)
+chdir(new_dir)
+
+if not path.exists(pcad_dir):
+    mkdir(pcad_dir)
+chdir(pcad_dir)
+# execfile('/home/aarvai/python/quarterlies/pcad_quarterly.py')
+chdir('..')
+
+if not path.exists(prop_dir):
+    mkdir(prop_dir)
+chdir(prop_dir)
+# execfile('/home/aarvai/python/quarterlies/prop_quarterly.py')
+chdir('../..')
+pp.close('all')
