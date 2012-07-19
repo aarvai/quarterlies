4722adbec0041a2869cbecf2635652ae7589935e
aarvai <aarvai@cfa.harvard.edu>
2012-07-19 16:11:20 -0400

    Added cust_title, samefig, fig_height, and fig_width to LTTplot
    Also changed limit_lines to plot_limits

--- run_both.py
+++ run_both.py
@@ -9,13 +9,13 @@ import filter_times as bad
 ##-------------------------------------------------------------
 # Inputs
 
-start = '2012:032:00:00:00'
-stop = '2012:150:00:00:00'
+start = '2011:212:00:00:00'
+stop = '2012:031:00:00:00'
 
 ##-------------------------------------------------------------
 # LTTplot function
 
-def LTTplot_test(var, **kwargs): 
+def LTTplot(var, **kwargs): 
     """Plot the daily min, max, and mean of a parameter, filtering for known
     bad data as listed in filter_times.py.
     
@@ -24,29 +24,39 @@ def LTTplot_test(var, **kwargs):
     Optional inputs:
     :savefig:      Save and close figure (default is True)
     :saveas:       Name to save figure as (default is <MSID>.png)
+    :samefig:      Use current figure (default is False)
     :start:        Start time (default is 2000:001)
     :stop:         Stop time (default is None)
     :stat:         Statistic type ('5min' or 'daily', default is 'daily')
-    :limit_lines:  Plot database yellow caution and red warning limits 
-                   (default is True)
-    :yellow:       User-defined yellow caution limit lines (default is none)
-    :red:          User-defined red warning limit lines  (default is none) 
-    :ylim:         User-defined  y-limits (default is none)
     :filter:       User-defined Start and End times to filter out due to bad 
                    data.  If none are supplied, it will default to any listed 
-                   in filter_times.py.  In addition, LTTplot will always filter
-                   known NSM and SSM events as listed in filter_times.py (bad_all).
-    :subplot:      Subplot information in list form.  If not supplied, a new
-                   figure will be created.
-    :cust_unit:    Custom unit to be displayed on y-axis label, typically for use 
-                   with custom_mult or derived parameters (default is none)
-    :custom_mult:  Custom multiplier, typically for use with custom unit 
-                   (default is none)   
+                   in filter_times.py.  In addition, LTTplot will always 
+                   filter known NSM and SSM events as listed in 
+                   filter_times.py (bad_all).
     :plot_stds:    Plot standard deviations (default is True)
     :plot_mins:    Plot minimum values (default is True)
     :plot_means:   Plot mean values (default is True)
     :plot_maxes:   Plot maximum values (default is True)
+    :plot_limits:  Plot database yellow caution and red warning limits 
+                   (default is True)
+    :yellow:       User-defined yellow caution limit lines (default is none)
+    :red:          User-defined red warning limit lines  (default is none)                
+    :subplot:      Subplot information in [rows, columns, subplot number] 
+                   (default is [1, 1, 1])
+    :fig_width:    Figure width (default is 8).  Helpful if x-axis is crowded.
+                   Irrelevant if samefig=True.
+    :fig_height:   Figure height (default varies by number of subplots:  
+                   6 for 1 row of subplots, 9 for 2 rows, otherwise 12).
+                   Irrelevant if samefig=True.
+    :ylim:         User-defined  y-limits (default is none)
+    
     :legend:       Display legend (default is False)
+    :cust_title:   Custom title (default is MSID and description)
+    :cust_unit:    Custom unit to be displayed on y-axis label, typically for 
+                   use with custom_mult or derived parameters (default is 
+                   none)
+    :custom_mult:  Custom multiplier, typically for use with custom unit 
+                   (default is none)   
     
     e.g.
     LTTplot('Dist_SatEarth')
@@ -54,10 +64,13 @@ def LTTplot_test(var, **kwargs):
     LTTplot('pr1tv01t', limit_lines=False, yellow=150, red=240)
     LTTplot('pr2tv01t', limit_lines=False, yellow=[40,150], red=[37,240])
     LTTplot('plaed3gt', filter=['2011:299:00:00:00 2011:300:00:00:00'])
-    LTTplot('pcm01t', subplot=[2,1,1], savefig=False) 
-    LTTplot('aogbias1', cust_unit='ARCSEC/SEC', cust_mult=3600*180/pi, subplot=311)
-    LTTplot('dp_css1_npm_sun', plot_means=False, plot_maxes=False, plot_stds=False, 
-            legend=True)
+    LTTplot('pcm01t', subplot=[4,1,1], savefig=False) 
+    LTTplot('aogbias1', cust_unit='ARCSEC/SEC', cust_mult=3600*180/pi, 
+            subplot=311)
+    LTTplot('aosares1', start='2003:001', stop='2003:100', fig_width=10, 
+            fig_height=6)
+    LTTplot('dp_css1_npm_sun', plot_means=False, plot_maxes=False, 
+             plot_stds=False, legend=True)
     """
     GRAY = '#999999'
     var = var.lower()
@@ -78,17 +91,21 @@ def LTTplot_test(var, **kwargs):
     # If plotting standard deviations, ax1 will only be used to reference default
     # axes size.  ax1 will be deleted and data will be plotted on ax2.
     sub = kwargs.pop('subplot', 111)
+    # Convert subplot input to list form if provided by user as integer
     if isscalar(sub):
         sub = [int(str(sub)[0]), int(str(sub)[1]), int(str(sub)[2])]
-    print sub
-    if sub[0] == 1:  
-        fig_height = 6
-    elif sub[0] == 2:  
-        fig_height = 9
-    else:  
-        fig_height = 12
-        
-    figure(figsize=(8, fig_height))
+    # Create a new figure unless using existing figure
+    if kwargs.pop('samefig', False):
+        if kwargs.has_key('fig_height'):
+            fig_height = kwargs.pop('fig_height')
+        elif sub[0] == 1:  
+            fig_height = 6
+        elif sub[0] == 2:  
+            fig_height = 9
+        else:  
+            fig_height = 12
+        fig_width = kwargs.pop('fig_width', 8)
+        figure(figsize=(fig_width, fig_height)
     ax1 = pp.subplot(sub[0], sub[1], sub[2])
     if kwargs.get('plot_stds', True):
         ax_pos = ax1.get_position().get_points()
@@ -100,31 +117,37 @@ def LTTplot_test(var, **kwargs):
     # Plot data
     mult = kwargs.pop('cust_mult', 1)    
     lim = kwargs.pop('limit_lines', True)
-    if mult!=1:
-        lim=False
     if kwargs.pop('plot_maxes', True):
         plot_cxctime(data.times, data.maxes * mult, 'k', label=(stat + ' maxes'))
     if kwargs.pop('plot_means', True):
         plot_cxctime(data.times, data.means * mult, 'g', label=(stat + ' means'))
     if kwargs.pop('plot_mins', True):
         plot_cxctime(data.times, data.mins * mult, color=GRAY, label=(stat + ' min'))
-    if hasattr(data, 'tdb'):
-        pp.title(data.msid.upper() + ':  ' + data.tdb.technical_name)
-        if lim == True:
-            # Check if single limit set exists in TDB
-            if size(data.tdb.Tlmt) == 1 and data.tdb.Tlmt is not None:
-                pp.plot(pp.xlim(), np.array([data.tdb.Tlmt[4], 
-                                             data.tdb.Tlmt[4]]), 'r')
-                pp.plot(pp.xlim(), np.array([data.tdb.Tlmt[2], 
-                                             data.tdb.Tlmt[2]]), 'y')
-                pp.plot(pp.xlim(), np.array([data.tdb.Tlmt[3], 
-                                             data.tdb.Tlmt[3]]), 'y')
-                pp.plot(pp.xlim(), np.array([data.tdb.Tlmt[5], 
-                                             data.tdb.Tlmt[5]]), 'r')  
-                if ~kwargs.has_key('ylim'):
-                    pp.ylim(np.array([data.tdb.Tlmt[4]-10.0, 
-                                      data.tdb.Tlmt[5]+10.0]))
-    else: pp.title(data.msid.upper())
+    
+    # Plot limits
+    if kwargs.pop('plot_limits', True):
+        # Check if single limit set exists in TDB
+        if hasattr(data, 'tdb') and size(data.tdb.Tlmt) == 1 and data.tdb.Tlmt is not None:
+            pp.plot(pp.xlim(), np.array([data.tdb.Tlmt[4] * mult, 
+                                         data.tdb.Tlmt[4] * mult]), 'r')
+            pp.plot(pp.xlim(), np.array([data.tdb.Tlmt[2] * mult, 
+                                         data.tdb.Tlmt[2] * mult]), 'y')
+            pp.plot(pp.xlim(), np.array([data.tdb.Tlmt[3] * mult, 
+                                         data.tdb.Tlmt[3] * mult]), 'y')
+            pp.plot(pp.xlim(), np.array([data.tdb.Tlmt[5] * mult, 
+                                         data.tdb.Tlmt[5] * mult]), 'r')  
+            if ~kwargs.has_key('ylim'):
+                pp.ylim(np.array([data.tdb.Tlmt[4]-10.0, 
+                                  data.tdb.Tlmt[5]+10.0]))
+    
+    # Add title
+    if kwargs.has_key('cust_title'):
+        title = kwargs.pop('cust_title')
+    elif hasattr(data, 'tdb'):
+        title = data.msid.upper() + ':  ' + data.tdb.technical_name
+    else:
+        title=data.msid.upper()    
+    pp.title(title)
     
     # Account for custom y-labels
     if kwargs.has_key('cust_unit'):
@@ -162,7 +185,7 @@ def LTTplot_test(var, **kwargs):
     if s == True:
         figname = kwargs.pop('saveas', data.msid.lower() + '.png')
         pp.savefig(figname)
-        pp.close()
+        #pp.close()
 
 
 ##-------------------------------------------------------------
@@ -188,6 +211,6 @@ chdir('..')
 if not path.exists(prop_dir):
     mkdir(prop_dir)
 chdir(prop_dir)
-execfile('/home/aarvai/python/quarterlies/prop_quarterly.py')
+#execfile('/home/aarvai/python/quarterlies/prop_quarterly.py')
 chdir('../..')
-pp.close('all')
+#pp.close('all')
