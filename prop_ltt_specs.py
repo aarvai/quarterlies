9be8f5d8f73cef6674aec9975c465e4c02c1a7be
aarvai <aarvai@cfa.harvard.edu>
2012-07-25 15:07:29 -0400

    Renamed pcad_plots.py to pcad_ltt_specs.py, prop_plots.py to prop_ltt_specs
    Clearer names.  See recent commits for other affected files.

---
+++ prop_ltt_specs.py
@@ -0,0 +1,288 @@
+plot_pcm01t =   {'ylim': [50,170], 
+                 'plot_limits': False, 
+                 'yellow': [85,145], 
+                 'red': [80,160]}
+
+plot_pcm02t =   {'ylim': [50,170], 
+                 'plot_limits': False, 
+                 'yellow': [85,145], 
+                 'red': [80,160]}
+
+plot_pcm03t =   {'ylim': [50,170], 
+                 'plot_limits': False, 
+                 'yellow': [65,145], 
+                 'red': [60,160]}
+
+plot_pcm04t =   {'ylim': [50,170], 
+                 'plot_limits': False, 
+                 'yellow': [65,145], 
+                 'red': [60,160]}
+
+plot_pfdm101t = {}
+
+plot_pfdm102t = {}
+
+plot_pfdm201t = {}
+
+plot_pfdm202t = {}
+
+plot_pffp01t =  {'ylim': [30,250], 
+                 'plot_limits': False, 
+                 'yellow': [47,200],
+                 'red': [40,240]}
+
+plot_pftank1t = {'ylim': [30,130], 
+                 'plot_limits': False, 
+                 'yellow': [47,100], 
+                 'red': [40,120]}
+
+plot_pftank2t = {'ylim': [30,130], 
+                 'plot_limits': False, 
+                 'yellow': [47,100], 
+                 'red': [40,120]}
+
+plot_pftankip = {'plot_limits': False, 
+                 'yellow': [270, 296], 
+                 'red': [240, 300]}
+
+plot_pftankop = {'plot_limits': False, 
+                 'yellow': [270, 296], 
+                 'red': [240, 300]}
+
+plot_phetankp = {}
+
+plot_phetankt = {'ylim': [-20,150], 
+                 'plot_limits': False, 
+                 'yellow': [0,130], 
+                 'red': [-10,140]}
+
+plot_phofp1t =  {'plot_limits': False, 
+                 'yellow': [35, 110], 
+                 'red': [30, 120]}
+
+plot_plaed1at = {'ylim': [-90,300]}
+
+plot_plaed1bt = {'ylim': [-90,300]}
+
+plot_plaed1ct = {'ylim': [-90,300]}
+
+plot_plaed1dt = {'ylim': [-90,300]}
+
+plot_plaed1et = {}
+
+plot_plaed1ft = {'ylim': [-90,300]}
+
+plot_plaed1gt = {'ylim': [-90,300]}
+
+plot_plaed1ht = {'ylim': [-90,300]}
+
+plot_plaed1it = {}
+
+plot_plaed2at = {'ylim': [-90,300]}
+
+plot_plaed2bt = {'ylim': [-90,300]}
+
+plot_plaed2ct = {}
+
+plot_plaed2dt = {}
+
+plot_plaed2et = {'ylim': [-90,300]}
+
+plot_plaed2ft = {}
+
+plot_plaed2gt = {}
+
+plot_plaed2ht = {'ylim': [-90,300]}
+
+plot_plaed2it = {'ylim': [-90,300]}
+
+plot_plaed3at = {'ylim': [-90,100]}
+
+plot_plaed3bt = {'ylim': [-90,100]}
+
+plot_plaed3ct = {'ylim': [-90,100]}
+
+plot_plaed3dt = {'ylim': [-90,100]}
+
+plot_plaed3et = {}
+
+plot_plaed3ft = {}
+
+plot_plaed3gt = {'ylim': [-90,100]}
+
+plot_plaed3ht = {}
+
+plot_plaed3it = {'ylim': [-90,100]}
+
+plot_plaed4at = {'ylim': [-90,100]}
+
+plot_plaed4bt = {'ylim': [-90,100]}
+
+plot_plaed4ct = {}
+
+plot_plaed4dt = {'ylim': [-90,100]}
+
+plot_plaed4et = {'ylim': [-90,100]}
+
+plot_plaed4ft = {'ylim': [-90,100]}
+
+plot_plaed4gt = {'ylim': [-90,100]}
+
+plot_plaed4ht = {'ylim': [-90,100]}
+
+plot_plaed4it = {'ylim': [-90,100]}
+
+plot_plaev1at = {'ylim': [30,250], 
+                 'plot_limits': False, 
+                 'yellow': [47,225], 
+                 'red': [40,240]}
+
+plot_plaev1bt = {'ylim': [30,250], 
+                 'plot_limits': False, 
+                 'yellow': [47,225], 
+                 'red': [40,240]}
+
+plot_plaev2at = {'ylim': [30,250], 
+                 'plot_limits': False, 
+                 'yellow': [47,225], 
+                 'red': [40,240]}
+
+plot_plaev2bt = {'ylim': [30,250], 
+                 'plot_limits': False, 
+                 'yellow': [47,225], 
+                 'red': [40,240]}
+
+plot_plaev3at = {'ylim': [30,250], 
+                 'plot_limits': False, 
+                 'yellow': [47,225], 
+                 'red': [40,240]}
+
+plot_plaev3bt = {'ylim': [30,250], 
+                 'plot_limits': False, 
+                 'yellow': [47,225], 
+                 'red': [40,240]}
+
+plot_plaev4at = {'ylim': [30,250], 
+                 'plot_limits': False, 
+                 'yellow': [-200,225], 
+                 'red': [-300,240]}
+
+plot_plaev4bt = {'ylim': [30,250], 
+                 'plot_limits': False, 
+                 'yellow': [47,225], 
+                 'red': [40,240]}
+
+plot_pline01t = {}
+
+plot_pline02t = {'ylim': [30,190], 
+                 'plot_limits': False, 
+                 'yellow': [47,170], 
+                 'red': [40,180]}
+
+plot_pline03t = {'ylim': [29.5,190], 
+                 'plot_limits': False, 
+                 'yellow': [42.5,170], 
+                 'red': [39.5,180]}
+
+plot_pline04t = {'ylim': [29.5,190], 
+                 'plot_limits': False, 
+                 'yellow': [42.5,170], 
+                 'red': [39.5,180]}
+
+plot_pline05t = {'ylim': [30,250], 
+                 'plot_limits': False, 
+                 'yellow': [47,200], 
+                 'red': [40,240]}
+
+plot_pline06t = {}
+
+plot_pline07t = {'ylim': [30,250], 
+                 'plot_limits': False, 
+                 'yellow': [47,200], 
+                 'red': [40,240]}
+
+plot_pline08t = {}
+
+plot_pline09t = {}
+
+plot_pline10t = {}
+
+plot_pline11t = {}
+
+plot_pline12t = {}
+
+plot_pline13t = {}
+
+plot_pline14t = {}
+
+plot_pline15t = {}
+
+plot_pline16t = {}
+
+plot_pm1thv1t = {}
+
+plot_pm1thv2t = {}
+
+plot_pm2thv1t = {'ylim': [30,250], 
+                 'plot_limits': False, 
+                 'yellow': [47,195], 
+                 'red': [40,240]}
+
+plot_pm2thv2t = {}
+
+plot_pm3thv1t = {}
+
+plot_pm3thv2t = {}
+
+plot_pm4thv1t = {}
+
+plot_pm4thv2t = {}
+
+plot_pmfp01t =  {'ylim': [30,150], 
+                 'plot_limits': False, 
+                 'yellow': [47,130], 
+                 'red': [40,140]}
+
+plot_pmtank1t = {}
+
+plot_pmtank2t = {}
+
+plot_pmtank3t = {}
+
+plot_pmtankp =  {}
+
+plot_pr1tv01t = {'plot_limits': False, 
+                 'yellow': [40, 195], 
+                 'red': [37, 240]}
+
+plot_pr1tv02t = {'plot_limits': False, 
+                 'yellow': [40, 195], 
+                 'red': [37, 240]}
+
+plot_pr2tv01t = {'plot_limits': False, 
+                 'yellow': [40, 195], 
+                 'red': [37, 240]}
+
+plot_pr2tv02t = {'plot_limits': False, 
+                 'yellow': [40, 195], 
+                 'red': [37, 240]}
+
+plot_pr3tv01t = {}
+
+plot_pr3tv02t = {}
+
+plot_pr4tv01t = {}
+
+plot_pr4tv02t = {}
+
+plot_pxdm01t =  {}
+
+plot_pxdm02t =  {}
+
+plot_pxtank1t = {}
+
+plot_pxtank2t = {}
+
+plot_pxtankip = {}
+
+plot_pxtankop = {}
