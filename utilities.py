04215dc996adfd7cecc4cb3c040e118988207b28
unknown <aarvai@ARVAI-LAPTOP.occ.harvard.edu>
2014-02-17 09:19:01 -0500

    Added new functions used by write_biases and htr_dc

--- utilities.py
+++ utilities.py
@@ -33,4 +33,165 @@ def reshape_array(array, num_rows, order='F'):
         out = array.reshape((num_rows, len(stdev) / int(num_rows)), order=order)
     return out
 
+def count_by_month(times):
+    """Counts the number of times an event occurs each month.
+    Inputs a set of DateTime times (in any format). 
+    Outputs an array of DateTime times spanning the timeframe with one entry per month
+    and the count of events that occur within that month
+    """    
+    len_mo = 365.25/12*24*3600
+
+    #Create an array that spans the timeframe with one date in the middle-ish of every month
+    x_start = DateTime(min(DateTime(times).secs)).iso[:7] + '-15 12:00:00.00'
+    x_stop = DateTime(max(DateTime(times).secs)).iso[:7] + '-15 12:00:00.00'
+    x_times = arange(DateTime(x_start).secs, DateTime(x_stop).secs, len_mo) 
     
+    #Collect the year and month for each date
+    t_month = [DateTime(t).iso[:7] for t in times]
+    x_month = [DateTime(x).iso[:7] for x in x_times]
+    
+    num_months = len(x_times)
+    month_counts = zeros(num_months)
+    for i in arange(num_months):
+        match = [t == x_month[i] for t in t_month]
+        month_counts[i] = sum(match)
+    month_times = x_times
+    return month_times, month_counts
+
+def sum_by_month(times, vals):
+    """Sums events by month.
+    Inputs a set of DateTime times (in any format). 
+    Outputs an array of DateTime times spanning the timeframe with one entry per month
+    and the sum of events that occur within that month
+    """    
+    len_mo = 365.25/12*24*3600
+
+    #Create an array that spans the timeframe with one date in the middle-ish of every month
+    x_start = DateTime(min(DateTime(times).secs)).iso[:7] + '-15 12:00:00.00'
+    x_stop = DateTime(max(DateTime(times).secs)).iso[:7] + '-15 12:00:00.00'
+    x_times = arange(DateTime(x_start).secs, DateTime(x_stop).secs, len_mo) 
+    
+    #Collect the year and month for each date
+    t_month = [DateTime(t).iso[:7] for t in times]
+    x_month = [DateTime(x).iso[:7] for x in x_times]
+    
+    num_months = len(x_times)
+    month_sums = zeros(num_months)
+    for i in arange(num_months):
+        match = [t == x_month[i] for t in t_month]
+        month_sums[i] = sum(vals[match])
+    month_times = x_times
+    return month_times, month_sums   
+
+def mean_by_month(times, vals):
+    """Sums events by month.
+    Inputs a set of DateTime times (in any format). 
+    Outputs an array of DateTime times spanning the timeframe with one entry per month
+    and the sum of events that occur within that month
+    """    
+    len_mo = 365.25/12*24*3600
+
+    #Create an array that spans the timeframe with one date in the middle-ish of every month
+    x_start = DateTime(min(DateTime(times).secs)).iso[:7] + '-15 12:00:00.00'
+    x_stop = DateTime(max(DateTime(times).secs)).iso[:7] + '-15 12:00:00.00'
+    x_times = arange(DateTime(x_start).secs, DateTime(x_stop).secs, len_mo) 
+    
+    #Collect the year and month for each date
+    t_month = [DateTime(t).iso[:7] for t in times]
+    x_month = [DateTime(x).iso[:7] for x in x_times]
+    
+    num_months = len(x_times)
+    month_means = zeros(num_months)
+    for i in arange(num_months):
+        match = [t == x_month[i] for t in t_month]
+        month_means[i] = mean(vals[match])
+    month_times = x_times
+    return month_times, month_means   
+    
+def heat_map(x, y, bins=(20,20), colorbar=True, **kwargs):
+    """Plots a "heat map", i.e. a scatter plot highlighting density
+
+    Based on an exmple from 
+    http://www.physics.ucdavis.edu/~dwittman/Matplotlib-examples/
+
+    Inputs: 
+    x = x vals (as from a scatter plot)
+    y = y vals (as from a scatter plot)
+    bins = int or tuple bins to feed into histogram2d
+    colorbar = whether or not to display colorbar (default=True)
+    x_lim = x limits of plot
+    y_lim = y_limits of plot
+    """
+    if kwargs.has_key('x_lim'):
+        x_lim = kwargs.pop('x_lim')
+    else:  
+        x_lim = [np.min(x), np.max(x)]
+    if kwargs.has_key('y_lim'):
+        y_lim = kwargs.pop('y_lim')
+    else:  
+        y_lim = [min(y), max(y)]
+    hist,xedges,yedges = np.histogram2d(x,y,bins=bins,range=[x_lim,y_lim])
+    extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+    pp.imshow(hist.T,extent=extent,interpolation='nearest',origin='lower', aspect='auto')
+    if colorbar==True:
+        pp.colorbar()
+    pp.show()    
+
+def same_limits(subs):
+    """Sets the x-limits and y-limits for a given set of subplots
+    equal to the collective mins and maxes
+    
+    Inputs:  
+    subs = subplots (tuple of 3-digit subplot numbers)
+    
+    e.g.  same_limits((411, 412, 413, 414))
+    """    
+    all_limits = np.zeros((len(subs), 4))
+    for sub, i in zip(subs, range(len(subs))):
+        pp.subplot(sub)
+        all_limits[i,:] = pp.axis()
+    best_limits = (min(all_limits[:,0]), max(all_limits[:,1]), 
+                   min(all_limits[:,2]), max(all_limits[:,3]))
+    for i in subs:
+        pp.subplot(i)
+        pp.axis(best_limits)
+
+def append_to_array(a, pos=-1, val=0):
+    """Appends a zero (or user-defined value) to a given one-dimensional array, 
+    either at the end (pos=-1) or beginning (pos=0).
+    
+    e.g. append_to_array(arange(5),pos=-1)
+    returns array([0, 1, 2, 3, 4, 0])
+    """
+    val_a = np.array([val])
+    if pos==0:
+        out = np.concatenate([val_a, a])
+    elif pos==-1:
+        out = np.concatenate([a, val_a])
+    return out    
+
+def find_closest(a, b):
+    """This function returns an array of length a with the indices of 
+    array b that are closest to the values of array a.
+    """
+    a = np.atleast_1d(np.array(a))
+    b = np.atleast_1d(np.array(b))
+    out = [np.argmin(abs(b - a1)) for a1 in a]
+    return out
+    
+def find_last_before(a, b):
+    """This function returns an array of length a with the indices of 
+    array b that are closest without going over the values of array a.
+    (Bob Barker rules.  Assumes b is sorted by magnitude.)
+    """
+    out = np.searchsorted(b,a,side='left') - 1
+    out[out==-1] = 0
+    return out    
+     
+def find_first_after(a, b):
+    """This function returns an array of length a with the indices of 
+    array b that are closest without being less than the values of array a.
+    (Opposite of Bob Barker rules.  Assumes b is sorted by magnitude.)
+    """
+    out = np.searchsorted(b,a,side='right')
+    return out      
