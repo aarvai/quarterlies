31d04a73919308cc558df7a0c99ade4a9cbd1f14
unknown <aarvai@ARVAI-LAPTOP.occ.harvard.edu>
2014-08-19 12:47:46 -0400

    Changed default start time, condensed directories, removed dc & bias data

--- run_quarterlies.py
+++ run_quarterlies.py
@@ -11,8 +11,8 @@ from utilities import mkdir_cd
 ##-------------------------------------------------------------
 # Inputs - Quarter start and stop
 
-start = '2013:213:00:00:00'
-stop = '2014:031:00:00:00'
+start = '2014:032:00:00:00'
+stop = '2014:212:00:00:00'
 
 ##-------------------------------------------------------------
 # Run Quarterlies
@@ -20,37 +20,33 @@ stop = '2014:031:00:00:00'
 # Define directory names
 t = Time.DateTime()
 new_dir = t.date[:4] + '_' + t.date[5:8] + '_RUN'
-pcad_dir = 'pcad_' + t.date[:4] + '_' + t.date[5:8]
-prop_dir = 'prop_' + t.date[:4] + '_' + t.date[5:8]
 
 # Make directories and create plots
 pp.close('all')
 mkdir_cd(new_dir)
-mkdir_cd(pcad_dir)
-mkdir_cd('mission')
-ltt.pcad_ltts('2000:001', stop)
-pcad_plots.fss('2000:001', stop)
-#pcad_plots.pointing_stab('pitch', '2000:001:00:00:00', stop)
-#pcad_plots.pointing_stab('yaw', '2000:001:00:00:00', stop)
-#pcad_plots.cmd_vs_act_torque('2000:001', stop)
+mkdir_cd('pcad_mission')
+ltt.pcad_ltts('1999:250', stop)
+pcad_plots.fss('1999:250', stop)
+#pcad_plots.pointing_stab('pitch', '1999:250:00:00:00', stop)
+#pcad_plots.pointing_stab('yaw', '1999:250:00:00:00', stop)
+#pcad_plots.cmd_vs_act_torque('1999:250', stop)
 pcad_plots.drag_torque('2000:001', stop, plot_months=False)
-mkdir_cd('../quarter')
+mkdir_cd('../pcad_quarter')
 ltt.pcad_ltts(start, stop)
 pcad_plots.fss(start, stop)
 #pcad_plots.pointing_stab('pitch', start, stop)
 #pcad_plots.pointing_stab('yaw', start, stop)
 #pcad_plots.cmd_vs_act_torque(start, stop)
-mkdir_cd('../../' + prop_dir)
-mkdir_cd('mission')
+mkdir_cd('../prop_mission')
 ltt.prop_ltts('2002:001:00:00:00', stop)
-prop_plots.htr_dc('PM3THV2T', on_temp=60, off_temp=89) #this step adds ~5 min
 mkdir_cd('zoom')
 ltt.prop_zoom_ltts('2002:001:00:00:00', stop)
-mkdir_cd('../../other')
+mkdir_cd('../../prop_other')
 prop_plots.mups_tank(stop)
+prop_plots.ips_tank(stop)
 prop_plots.therm_dropouts(stop)
+prop_plots.fdm2(stop)
 chdir('../..')
 pp.close('all')
-pcad_plots.collect_bias_data(stop) #this step adds ~10 min
 
 
